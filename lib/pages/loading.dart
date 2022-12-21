4d8d736209da0450fffbb95adb744de39eafaa86
ANSHIKA010 <sanshikas010@gmail.com>
2022-12-21 20:56:57 +0530

    World time Custom class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
+
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -11,19 +11,15 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async{
-    Response response = await get(Uri.https('worldtimeapi.org','api/timezone/Asia/Kolkata'));
-    Map data = jsonDecode(response.body);
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'];
-     DateTime now  = DateTime.parse(datetime);
-     print(now);
-  }
+void setupWorldTime() async{
+  WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+  await instance.getTime();
+}
 
   @override
   void initState() {
     super.initState();
-    getTime();
+   setupWorldTime();
   }
 
 
