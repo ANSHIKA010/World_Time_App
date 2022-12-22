087cbfc79e3ff578201cfb1424fb160a143cd3df
ANSHIKA010 <sanshikas010@gmail.com>
2022-12-22 11:46:20 +0530

    World time Custom class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,9 +11,15 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  String time = 'loading';
+
 void setupWorldTime() async{
   WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
   await instance.getTime();
+  print(instance.time);
+  setState(() {
+    time = instance.time;
+  });
 }
 
   @override
@@ -28,7 +34,10 @@ void setupWorldTime() async{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      )
     );
   }
 }
